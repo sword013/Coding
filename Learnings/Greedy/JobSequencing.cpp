a52aaa5cea3de1775ab213a8f8983773fffb23aa
Tejas Deshmukh <tejasdeshmukh15692@gmail.com>
2024-10-30 11:38:46 +0530

    Update JobSequencing.cpp

--- Learnings/Greedy/JobSequencing.cpp
+++ Learnings/Greedy/JobSequencing.cpp
@@ -1,7 +1,7 @@
 //https://www.geeksforgeeks.org/problems/job-sequencing-problem-1587115620/1
 /*
     Learnings : Via normal Disjoint set this will not work. You will have to change union function
-    a little bit so that it works. Take and example and see that slot-1 is to be made parent of slot
+    a little bit so that it works. Take an example and see that slot-1 is to be made parent of slot
     no matter what size or rank. so this union doesnt depend on size or rank. its purely 
     parent[slot] = (slot-1) or (slot-1)'s ult parent;
 */
@@ -42,7 +42,6 @@ class DisjointSet{
   vector<int> parent;
     DisjointSet(int n){
         parent.resize(n+1);
-        //size.resize(n+1,1);
         for(int i=0;i<=n;i++){
             parent[i]=i;
         }
@@ -71,14 +70,11 @@ class Solution {
         for(int i=0;i<n;i++) md=max(md,jobs[i].deadline);
         
         DisjointSet ds(md);
-        //3,5 -> 0(taken) ; 1,2,4,6-> not taken
         int profit=0,n_jobs=0;
         for(int i=0;i<n;i++){
             int d = jobs[i].deadline;
             int slot = ds.findUPar(d);
-            //cout<<"getting slot as : " <<slot <<endl;
             if(slot>0) {
-                //cout<<"doing union on " << slot<< " and " <<slot-1<<endl;
                 ds.unionCustom(ds.findUPar(slot-1),slot);
                 n_jobs++;
                 profit+=jobs[i].profit;
