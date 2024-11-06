2147046fe15b6a074ec7d986f871e3d1aa38df4f
Tejas Deshmukh <tejasdeshmukh15692@gmail.com>
2024-11-06 22:26:06 +0530

    Update Queries.sql

--- SQL Queries/Queries.sql
+++ SQL Queries/Queries.sql
@@ -1,2 +1,6 @@
-1) select max(salary) as SecondHighestSalary from Employee where salary not in (select max(salary) from Employee);
+1) Leetcode 176. Second Highest Salary 
+select max(salary) as SecondHighestSalary from Employee where salary not in (select max(salary) from Employee);
 
+select max(salary) as SecondHighestSalary from Employee where salary<(select max(salary) from Employee);
+
+2)
